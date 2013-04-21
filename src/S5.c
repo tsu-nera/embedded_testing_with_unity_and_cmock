dccab5f742312d0b98eb5813d0fd039e8a8d8a9b
tsu-nera <fox10225fox@gmail.com>
2013-04-21 16:12:28 +0900

    p18

--- src/S5.c
+++ src/S5.c
@@ -2,8 +2,12 @@
 
 void S5_Exec(void)
 {
-  if( Buttons_CheckS5Switch() == 0 )
+  if( Buttons_CheckS5Switch() )
   {
+    S5Ctrl_SetFrequency(20);
+    S5Ctrl_Loud();
+  }
+  else {
     S5Ctrl_Silence();
   }
 }
